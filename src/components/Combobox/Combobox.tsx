1b1967abaaf029fcc14092da24998fc43e4a303b
Ha Quanq <quangha0120010@gmail.com>
2025-10-13 20:52:53 +0700

    Inherit Button styles, sizes to Combobox

--- src/components/Combobox/Combobox.tsx
+++ src/components/Combobox/Combobox.tsx
@@ -2,6 +2,7 @@ import ChevronDownIcon from "@/assets/images/chevron-down.svg?react";
 import { cn } from "@/utils/cn";
 import { Select } from "radix-ui";
 import { useState } from "react";
+import { Button } from "../common/Button";
 
 interface ComboboxProps {
   data: string[];
@@ -10,9 +11,10 @@ interface ComboboxProps {
   prefix?: string;
   label: string;
   placeholder?: string;
+  size?: "small" | "normal" | "large";
 }
 
-export const Combobox = (props: ComboboxProps) => {
+export const Combobox = ({ size = "normal", ...props }: ComboboxProps) => {
   const [value, setValue] = useState(props.defaultValue);
   const [open, setOpen] = useState(false);
 
@@ -27,20 +29,22 @@ export const Combobox = (props: ComboboxProps) => {
 
   return (
     <Select.Root onValueChange={handleValueChange} onOpenChange={handleOpenChange} value={value}>
-      <Select.Trigger className="font-semi-bold flex h-12 w-full items-center justify-between rounded-sm px-4 text-start text-lg text-gray-950 ring ring-gray-200 transition-[box-shadow] hover:ring-gray-950 dark:text-white dark:ring-blue-700 hover:dark:ring-white">
-        <Select.Value>
-          {value === props.defaultValue ? props.placeholder || props.label : props.prefix + value}
-        </Select.Value>
-        <Select.Icon>
-          <ChevronDownIcon className={cn("transition-transform", open && "rotate-180")} />
-        </Select.Icon>
+      <Select.Trigger className="w-full rounded-sm">
+        <Button asWrapper="div" size={size} className="justify-between">
+          <Select.Value>
+            {value === props.defaultValue ? props.placeholder || props.label : props.prefix + value}
+          </Select.Value>
+          <Select.Icon>
+            <ChevronDownIcon className={cn("transition-transform", open && "rotate-180")} />
+          </Select.Icon>
+        </Button>
       </Select.Trigger>
       <Select.Portal>
         <Select.Content className="z-50 overflow-hidden rounded-sm bg-white shadow-2xl" position="popper">
           <Select.ScrollUpButton />
           <Select.Viewport className="overflow-auto">
             {props.data.map((value) => (
-              <SelectItem value={value} key={value}>
+              <SelectItem size={size} value={value} key={value}>
                 {value}
               </SelectItem>
             ))}
@@ -52,17 +56,20 @@ export const Combobox = (props: ComboboxProps) => {
   );
 };
 
-const SelectItem = ({ children, className, ...props }: Select.SelectItemProps) => {
+interface ComboboxItemProps extends Select.SelectItemProps {
+  size: "small" | "normal" | "large";
+}
+
+const SelectItem = ({ children, size, ...props }: ComboboxItemProps) => {
   return (
     <Select.Item
-      className={cn(
-        "font-semi-bold flex h-12 min-w-40 cursor-pointer items-center justify-between gap-1 px-4 text-gray-950 capitalize transition-colors outline-none hover:bg-gray-100 focus-visible:bg-gray-100 dark:bg-blue-900 dark:text-white hover:dark:bg-blue-800 focus-visible:dark:bg-blue-800",
-        className,
-      )}
+      className="cursor-pointer bg-white outline-none hover:bg-gray-100 focus-visible:bg-gray-100 dark:bg-blue-900 dark:hover:bg-blue-800 dark:focus-visible:bg-blue-800"
       {...props}
     >
-      <Select.ItemText className="">{children}</Select.ItemText>
-      <Select.ItemIndicator className="h-1 w-3 rounded-full bg-gray-950 dark:bg-white"></Select.ItemIndicator>
+      <Button asWrapper="div" size={size} className="justify-between ring-0">
+        <Select.ItemText className="">{children}</Select.ItemText>
+        <Select.ItemIndicator className="h-1 w-3 rounded-full bg-gray-950 dark:bg-white"></Select.ItemIndicator>
+      </Button>
     </Select.Item>
   );
 };
