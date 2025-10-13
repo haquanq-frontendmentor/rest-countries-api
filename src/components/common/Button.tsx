ec8da3e693c6e3443efcedbbe899921a913bbd96
Ha Quanq <quangha0120010@gmail.com>
2025-10-13 20:43:26 +0700

    Add Button sizes and styles inheritance through div/span

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,16 +1,28 @@
 import { cn } from "@/utils/cn";
+import { createElement } from "react";
 
-interface ButtonProps extends React.ComponentProps<"button"> {}
+interface ButtonProps extends React.ComponentProps<"button"> {
+  size?: "small" | "normal" | "large";
+  asWrapper?: "div" | "span";
+}
 
-export const Button = ({ children, className, ...restProps }: ButtonProps) => {
+export const Button = ({ children, className, size = "normal", asWrapper, ...restProps }: ButtonProps) => {
+  const baseClass = cn(
+    "font-semi-bold rounded-sm flex items-center bg-transparent text-gray-950 ring ring-gray-200 transition-[color,box-shadow] hover:ring-gray-950 dark:text-white dark:ring-blue-700 hover:dark:ring-white",
+    {
+      "h-8 px-3 text-sm": size === "small",
+      "h-10 px-4 text-base": size === "normal",
+      "h-12 px-6 text-lg": size === "large",
+    },
+    className,
+  );
+
+  if (asWrapper) {
+    return createElement(asWrapper, { className: baseClass, children, ...restProps });
+  }
+
   return (
-    <button
-      className={cn(
-        "font-semi-bold h-10 rounded-sm bg-transparent px-4 text-gray-950 ring ring-gray-200 transition-[color,box-shadow] hover:ring-gray-950 dark:text-white dark:ring-blue-700 hover:dark:ring-white",
-        className,
-      )}
-      {...restProps}
-    >
+    <button className={baseClass} {...restProps}>
       {children}
     </button>
   );
