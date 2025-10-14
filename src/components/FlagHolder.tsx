530ea207c86ff7032df29805f5fe1e53b310149e
Ha Quanq <quangha0120010@gmail.com>
2025-10-14 17:19:40 +0700

    Remove some style to make component generic

--- src/components/FlagHolder.tsx
+++ src/components/FlagHolder.tsx
@@ -11,9 +11,9 @@ export const FlagHolder = ({ src, alt, className }: FlagHolderProps) => {
   const Flag = (props: React.ComponentProps<"img">) => createElement("img", { src, alt, ...props });
 
   return (
-    <span className={cn("relative flex items-center justify-center overflow-hidden rounded-md p-4", className)}>
-      <Flag className="relative z-10 h-full object-contain transition-[scale] duration-500 group-hover:scale-110" />
-      <Flag className="absolute inset-0 h-full object-cover blur-[100px] brightness-75" />
+    <span className={cn("relative flex items-center justify-center overflow-hidden rounded-[inherit] p-4", className)}>
+      <Flag className="relative z-10 h-full object-contain" />
+      <Flag className="absolute inset-0 h-full w-full object-cover blur-[100px] brightness-75" />
     </span>
   );
 };
