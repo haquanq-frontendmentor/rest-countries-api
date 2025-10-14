e72538acabea9ef0f50aea944c371fab060514c5
Ha Quanq <quangha0120010@gmail.com>
2025-10-15 01:12:37 +0700

    Refactor FlagHolder, code clean up

--- src/components/FlagHolder.tsx
+++ src/components/FlagHolder.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/utils/cn";
-import { createElement } from "react";
 
 interface FlagHolderProps {
   src: string;
@@ -8,12 +7,10 @@ interface FlagHolderProps {
 }
 
 export const FlagHolder = ({ src, alt, className }: FlagHolderProps) => {
-  const Flag = (props: React.ComponentProps<"img">) => createElement("img", { src, alt, ...props });
-
   return (
     <span className={cn("relative flex items-center justify-center overflow-hidden rounded-[inherit] p-4", className)}>
-      <Flag className="relative z-10 h-full object-contain" />
-      <Flag className="absolute inset-0 h-full w-full object-cover blur-[100px] brightness-75" />
+      <img className="relative z-10 h-full object-contain" src={src} alt={alt} />
+      <img className="absolute inset-0 h-full w-full object-cover blur-[100px] brightness-75" src={src} alt="" />
     </span>
   );
 };
