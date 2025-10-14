857d3b3e4c6dd7c9f243ca61c370d60700185537
Ha Quanq <quangha0120010@gmail.com>
2025-10-14 15:51:48 +0700

    Add DefinitionList classname props

--- src/components/Definition/Definition.tsx
+++ src/components/Definition/Definition.tsx
@@ -3,13 +3,14 @@ import { DefinitionList } from "./DefinitionList";
 interface DefinitionProps {
   data: { name: string; description: string }[];
   label: React.JSX.Element;
+  className?: string;
 }
 
-export const Definition = ({ data, label }: DefinitionProps) => {
+export const Definition = ({ data, label, className }: DefinitionProps) => {
   return (
-    <section className="text-gray-950 dark:text-white">
+    <section>
       {label}
-      <DefinitionList data={data} />
+      <DefinitionList data={data} className={className} />
     </section>
   );
 };
--- src/components/Definition/DefinitionList.tsx
+++ src/components/Definition/DefinitionList.tsx
@@ -1,12 +1,16 @@
+import { cn } from "@/utils/cn";
 import { DefinitionItem } from "./DefinitionItem";
 
 interface DefinitionListProps {
   data: { name: string; description: string }[];
+  className?: string;
 }
 
-export const DefinitionList = ({ data }: DefinitionListProps) => {
+export const DefinitionList = ({ data, className }: DefinitionListProps) => {
   return (
-    <dl className="grid gap-1 leading-5 dark:border-t-blue-700">
+    <dl
+      className={cn("grid gap-1 text-base leading-5 text-gray-950 dark:border-t-blue-700 dark:text-white", className)}
+    >
       {data.map((v) => (
         <DefinitionItem {...v} key={v.name + v.description} />
       ))}
