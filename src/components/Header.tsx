cbdaaa0badd0b79ea92731ebcb4e94dfa038b9a4
Ha Quanq <quangha0120010@gmail.com>
2025-10-15 01:40:55 +0700

    Update page title when viewing country detail

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,24 @@
 import MoonStarIcon from "@/assets/images/moon-star.svg?react";
 import SunIcon from "@/assets/images/sun.svg?react";
+import { getCountryCodeBySlug, useCountryStore } from "@/stores/countryStore";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 import { Container } from "../layouts/Container";
 
 export const Header = () => {
   const USER_SAVED_THEME_KEY = "user-pref-theme";
   const HTML_THEME_KEY = "data-theme";
+  const DEFAULT_TITLE = "Where in the world?";
 
   const isUserPreferDarkMode = () => window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
 
   const [theme, setTheme] = useState(
     localStorage.getItem(USER_SAVED_THEME_KEY) || (isUserPreferDarkMode() ? "dark" : "light"),
   );
+  const [title, setTitle] = useState(DEFAULT_TITLE);
+
+  const { pathname } = useLocation();
+  const countries = useCountryStore((state) => state.countries);
 
   const saveTheme = () => {
     localStorage.setItem(USER_SAVED_THEME_KEY, theme);
@@ -26,11 +33,22 @@ export const Header = () => {
     saveTheme();
   }, [theme]);
 
+  useEffect(() => {
+    if (pathname != "/") {
+      const countryCode = getCountryCodeBySlug(pathname.slice(1));
+      const country = countries.find((v) => v.cca3 === countryCode);
+      if (!country) return;
+      setTitle(country.name.common);
+    } else {
+      setTitle(DEFAULT_TITLE);
+    }
+  }, [pathname]);
+
   return (
     <header className="bg-white text-gray-950 shadow-sm dark:bg-blue-900 dark:text-white">
       <Container>
         <div className="flex items-center justify-between py-6">
-          <h1 className="font-extra-bold text-2xl leading-8">Where in the world?</h1>
+          <h1 className="font-extra-bold text-2xl leading-8">{title}</h1>
           <button
             className="flex aspect-square w-8 items-center justify-center rounded-sm transition-[scale,color] active:scale-95"
             type="button"
